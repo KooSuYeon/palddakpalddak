d01b7043e1fdd4a15908859e27ae99dd9fb2fd8e
kyso8575 <kyso8575@gmail.com>
2024-11-22 09:38:52 +0900

    (fix) Move URLs to .env

--- LLM_RAG_data.py
+++ LLM_RAG_data.py
@@ -1,4 +1,5 @@
 import re
+import os
 import time
 import nltk
 from selenium import webdriver
@@ -10,6 +11,12 @@ from langchain.schema import Document
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import ssl
+
+from dotenv import load_dotenv
+
+
+load_dotenv()  # .env 파일에서 환경 변수 로드
+
 
 # NLTK에서 불용어와 토큰화에 필요한 리소스 다운로드
 
@@ -68,52 +75,53 @@ def fetch_page_content(urls):
 
     return all_texts
 
-# 여러 URL 리스트
-urls = [
-    # 1주차
-    "https://teamsparta.notion.site/LLM-bc8f1fe5e9cb4033a684af9b6432501e",
-    "https://teamsparta.notion.site/LLM-e9245bcecd8c4ffcb208de612425571d",
-    "https://teamsparta.notion.site/OpenAI-Playground-1b359405637349b4b658c9cd428b8454",
-    # 2주차
-    "https://teamsparta.notion.site/3678e2f29c004649880ecf030b4ea6a9",
-    "https://teamsparta.notion.site/85ee22b636bf402cb2603ae9c5ac2eab",
-    "https://teamsparta.notion.site/ea1d0042e2874b2eb122b3ae3234303e",
-    # 3주차
-    "https://teamsparta.notion.site/Shot-e30ba599508d40d7afd0ea674c6d917b",
-    "https://teamsparta.notion.site/Act-As-4fb832f337034a4ca462ba8fa6d74c34",
-    "https://teamsparta.notion.site/2ef1e307bd5c4766ac1c4fe82a5dd742",
-    # 4주차
-    "https://teamsparta.notion.site/0a629faa1d44461eb5d1f3413e6c701a",
-    "https://teamsparta.notion.site/32525b172df14fdfaf20f553e1be785e",
-    # 5주차
-    "https://teamsparta.notion.site/LLM-caa6bd3ca7af417d8c143384f7f61668",
-    "https://teamsparta.notion.site/Vector-DB-RAG-Retrieval-Augmented-Generation-f0d0d151ad0b422ab9939b6012469be8",
-    "https://teamsparta.notion.site/5b99d723739841e6b5010758801d5158",
-    "https://teamsparta.notion.site/LangChain-fa67277f776b48d59aa7e25e1f79980e",
-    "https://teamsparta.notion.site/Python-LangChain-FAISS-c8c90ebb26a34d59aa2b0cc8417c4dd2",
-    "https://teamsparta.notion.site/Sentence-Transformer-Word2Vec-Transformer-635fca3ce0784db592994b297959fc01",
-    "https://teamsparta.notion.site/a947d836cbae4770affc53b3bb8b3441"
-]
+def web_scrawl_contents():
+    # 여러 URL 리스트
+    urls = [
+        os.getenv('WEEK_1_LINK_1'),
+        os.getenv('WEEK_1_LINK_2'),
+        os.getenv('WEEK_1_LINK_3'),
+        os.getenv('WEEK_2_LINK_1'),
+        os.getenv('WEEK_2_LINK_2'),
+        os.getenv('WEEK_2_LINK_3'),
+        os.getenv('WEEK_3_LINK_1'),
+        os.getenv('WEEK_3_LINK_2'),
+        os.getenv('WEEK_3_LINK_3'),
+        os.getenv('WEEK_4_LINK_1'),
+        os.getenv('WEEK_4_LINK_2'),
+        os.getenv('WEEK_5_LINK_1'),
+        os.getenv('WEEK_5_LINK_2'),
+        os.getenv('WEEK_5_LINK_3'),
+        os.getenv('WEEK_5_LINK_4'),
+        os.getenv('WEEK_5_LINK_5'),
+        os.getenv('WEEK_5_LINK_6'),
+        os.getenv('WEEK_5_LINK_7')
 
-# 텍스트 추출
-texts = fetch_page_content(urls)
+    ]
 
-# RecursiveCharacterTextSplitter를 사용하여 텍스트 분할
-recursive_text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,  # 덩어리 크기
-    chunk_overlap=10,  # 덩어리 겹침 크기
-    length_function=len,
-    is_separator_regex=False,
-)
+    # 텍스트 추출
+    texts = fetch_page_content(urls)
 
-# 모든 텍스트를 Document 객체로 감싸서 분할
-documents = [Document(page_content=text) for text in texts]
+    # RecursiveCharacterTextSplitter를 사용하여 텍스트 분할
+    recursive_text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100,  # 덩어리 크기
+        chunk_overlap=10,  # 덩어리 겹침 크기
+        length_function=len,
+        is_separator_regex=False,
+    )
 
-# 텍스트 분할
-splits = []
-for doc in documents:
-    splits += recursive_text_splitter.split_documents([doc])  # Document 객체 리스트 전달
+    # 모든 텍스트를 Document 객체로 감싸서 분할
+    documents = [Document(page_content=text) for text in texts]
 
-# 분할된 텍스트 확인
-for idx, split in enumerate(splits[:10]):
-    print(f"Chunk {idx+1}: {split.page_content[:10]}..")  # 앞부분 50자만 출력
+    # 텍스트 분할
+    splits = []
+    for doc in documents:
+        splits += recursive_text_splitter.split_documents([doc])  # Document 객체 리스트 전달
+
+    # 분할된 텍스트 확인
+    for idx, split in enumerate(splits[:10]):
+        print(f"Chunk {idx+1}: {split.page_content[:10]}..")  # 앞부분 50자만 출력
+
+
+if __name__ == '__main__':
+    web_scrawl_contents()
