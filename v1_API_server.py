c906b8b9fb5543a37be695aad96ddc68f5a5c2e8
Teddy <157982964+sj5black@users.noreply.github.com>
2024-11-29 11:43:16 +0900

    Add/server (#17)
    * [DDAK-24] develop Client-Server API system
    
    * [DDAK-24] fix file name
    
    * [DDAK-24] integrate Client, server file

--- v1_API_server.py
+++ v1_API_server.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, validator
 from rag_model import get_question, get_feedback, get_session_no  # rag_model.py 파일을 임포트
+<<<<<<< HEAD
+from fastapi.responses import JSONResponse
+import os
+import glob
 import uvicorn
 import logging
+from datetime import datetime
+=======
+import uvicorn
+import logging
+>>>>>>> e9bd3b1ca79bf84529161dc57f935338f3e7d117
 
 ############ 로그 파일 생성 ######################
 logging.basicConfig(
@@ -29,6 +38,10 @@ order: int = 3
 
 # FastAPI 애플리케이션 생성
 app = FastAPI()
+<<<<<<< HEAD
+FILE_DIR = "./text_files"
+=======
+>>>>>>> e9bd3b1ca79bf84529161dc57f935338f3e7d117
 
 # CORS
 app.add_middleware(
@@ -66,6 +79,13 @@ class AnswerRequest(BaseModel):
 
 class TypeRequest(BaseModel):
     sidebox_type: str
+<<<<<<< HEAD
+
+class Conversation(BaseModel):
+    user_id: int
+    conversation: str
+=======
+>>>>>>> e9bd3b1ca79bf84529161dc57f935338f3e7d117
 ###############################################################
 ###################### 요청 메서드 처리 #######################
 ###############################################################
@@ -118,6 +138,50 @@ async def check_answer(request: AnswerRequest):
         logger.error(f"Unexpected error: {e}")
         raise HTTPException(status_code=500, detail="Internal server error.")
 
+<<<<<<< HEAD
+# 대화 불러오기 api
+@app.get("/get_history/{user_id}")
+async def get_history(user_id: int):
+    # 파일 경로 패턴
+    pattern = os.path.join(FILE_DIR, f"{user_id}_*.txt")
+    
+    # 패턴에 맞는 파일 찾기
+    file_paths = glob.glob(pattern)
+    
+    print(file_paths)
+    if not file_paths:
+        raise HTTPException(status_code=404, detail="Files not found")
+    
+    files_content = []
+    for file_path in file_paths:
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read()
+            files_content.append({
+                "file_name": os.path.basename(file_path),
+                "content": content
+            })
+    return JSONResponse(content=files_content)
+
+# 대화 저장 API
+@app.post("/save_conversation")
+async def save_conversation(conversation: Conversation):
+    # 현재 시간을 밀리초 단위로 포함하여 파일 이름 설정
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")  # 예: 20241126_153045_123456
+    file_name = f"{conversation.user_id}_{timestamp}.txt"
+    file_path = os.path.join(FILE_DIR, file_name)
+    
+    # 파일 디렉토리가 없으면 생성
+    if not os.path.exists(FILE_DIR):
+        os.makedirs(FILE_DIR)
+    
+    # 대화 내용 파일에 추가
+    with open(file_path, "a", encoding="utf-8") as file:
+        file.write(conversation.conversation)
+    
+    return {"message": "Conversation saved successfully."}
+
+=======
+>>>>>>> e9bd3b1ca79bf84529161dc57f935338f3e7d117
 # 메인 함수
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
